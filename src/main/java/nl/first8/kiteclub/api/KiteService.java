33453e7b92569b3e3f40ee9598be537a357c4792
Gerke Kok - First8 <gerke.kok@first8.nl>
2016-03-28 18:22:19 +0200

    get the employeeNumber from the otherClaims in the token

--- src/main/java/nl/first8/kiteclub/api/KiteService.java
+++ src/main/java/nl/first8/kiteclub/api/KiteService.java
@@ -1,5 +1,7 @@
 package nl.first8.kiteclub.api;
 
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Resource;
@@ -7,12 +9,17 @@ import javax.annotation.security.RolesAllowed;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
 
 import nl.first8.kiteclub.db.KiteDao;
 import nl.first8.kiteclub.domain.Kite;
@@ -47,6 +54,9 @@ public class KiteService {
 			return Response.status(Status.NOT_FOUND).encoding("Kite not found").build();
 		}
 
+		if (kite.getOwner() != getEmployeeNumber()) {
+			return Response.status(Status.FORBIDDEN).encoding("You are not the owner of this kite").build();
+		}
 		return Response.status(200).entity(kite).build();
 	}
 
@@ -58,8 +68,23 @@ public class KiteService {
 		return Response.status(200).entity(kiteDao.getByOwner(getEmployeeNumber())).build();
 	}
 
-	private int getEmployeeNumber() {
-		// TODO: read the employeeNumber from somewhere...
-		return 0;
+	Integer getEmployeeNumber() {
+		@SuppressWarnings("unchecked")
+		KeycloakPrincipal<KeycloakSecurityContext> p = (KeycloakPrincipal<KeycloakSecurityContext>) ctx
+				.getCallerPrincipal();
+		AccessToken token = p.getKeycloakSecurityContext().getToken();
+		Map<String, Object> otherClaims = token.getOtherClaims();
+		dumpClaims(otherClaims);
+		Object idObject = otherClaims.get(ATTR_ID);
+		if (idObject == null) {
+			throw new ForbiddenException("user-employeeNumber attribute is null -> not set");
+		}
+		return Integer.parseInt((String) idObject);
+	}
+
+	static void dumpClaims(Map<String, Object> otherClaims) {
+		for (String key : otherClaims.keySet()) {
+			LOG.log(Level.INFO, "Key {0}: value: {1}", new Object[] { key, otherClaims.get(key) });
+		}
 	}
 }
